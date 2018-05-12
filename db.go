898f4813a0ee05d0401fc621342866fa1bd7f86a
brondum <34370407+brondum@users.noreply.github.com>
2018-05-12 19:20:51 +0200

    Added db connect function

--- db.go
+++ db.go
@@ -7,24 +7,56 @@ import (
 	"github.com/golang-migrate/migrate"
 	"github.com/golang-migrate/migrate/database/postgres"
 	_ "github.com/golang-migrate/migrate/source/file"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"github.com/olling/slog"
 )
 
 func InitializeDBMigration() {
-	db, err := sql.Open("postgres", "postgres://"+Conf.DatabaseConf.User+":"+Conf.DatabaseConf.Password+"@"+Conf.DatabaseConf.Host+":"+strconv.Itoa(Conf.DatabaseConf.Port)+"/"+Conf.DatabaseConf.DatabaseName+"?sslmode=disable")
+	db, err := getDbConnection()
 	if err != nil {
 		slog.PrintError("could not connect to database: ", err)
 	}
+	defer db.Close()
+
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
 		slog.PrintError("Driver error: ", err)
 	}
+
 	m, err := migrate.NewWithDatabaseInstance(
 		"file://./database/migrations",
 		"postgres", driver)
 	if err != nil {
 		slog.PrintError("Migration error: ", err)
 	}
+
 	m.Up()
+}
+
+func bulkInsertRecipes(unsavedRows []name) error {
+	db := getDbConnection()
+	defer db.Close()
+
+	valueStrings := make([]string, 0, len(unsavedRows))
+	valueArgs := make([]interface{}, 0, len(unsavedRows)*3)
+	for _, post := range unsavedRows {
+		valueStrings = append(valueStrings, "(?, ?, ?)")
+		valueArgs = append(valueArgs, post.name)
+		valueArgs = append(valueArgs, post.name)
+		valueArgs = append(valueArgs, post.name)
+	}
+	stmt := fmt.Sprintf("INSERT INTO table (name, name, name) VALUES %s", strings.Join(valueStrings, ","))
+	_, err := db.Exec(stmt, valueArgs...)
+	return err
+}
+
+func getDbConnection() (*sqlx.DB, error) {
+	//Connect to database
+	db, err := sqlx.Connect("postgres", "postgres://"+Conf.DatabaseConf.User+":"+Conf.DatabaseConf.Password+"@"+Conf.DatabaseConf.Host+":"+strconv.Itoa(Conf.DatabaseConf.Port)+"/"+Conf.DatabaseConf.DatabaseName+"?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
