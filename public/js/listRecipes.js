c12d628b3a897906c450be0f8af385ece6bf8b1b
brondum <34370407+brondum@users.noreply.github.com>
2018-05-20 09:16:45 +0200

    Added initial web to list recipes (#4), handler still needs to be defined

---
+++ public/js/listRecipes.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+	$(function(){
+		$('#recipeListRefresh').click(function() {
+
+			$.ajax({
+					url: '/api/recipes',
+					type: "GET",
+					beforeSend: function (xhrObj) {
+					},
+					complete: function () {
+					},
+					success: function (data) {
+						$("#recipeList").empty();
+						$('#recipeList').append('<thead id="recipeListTableHead">\
+									<tr>\
+										<th>\
+											ID\
+										</th>\
+										<th>\
+											Title\
+										</th>\
+										<th>\
+											Added\
+										</th>\
+										<th>\
+											Blog\
+										</th>\
+										<th>\
+											Instructions\
+										</th>\
+									</tr>\
+								</thead>\
+								<tbody id="recipeListTableBody">\
+								</tbody>\
+		            		');
+						var isJSON = true;
+						try {
+							var response = JSON.parse(data);
+						}
+						catch (err) {
+							isJSON = false
+						}
+						if (isJSON == true) {
+							var recipes = JSON.parse(data);
+							jQuery.each(recipes, function (index, item) {
+								$('<tr>\
+										<td>\
+											'+ item.id + '\
+										</td>\
+										<td>\
+											'+ item.title + '\
+										</td>\
+										<td>\
+											'+ item.added + '\
+										</td>\
+										<td>\
+											'+ item.blog + '\
+										</td>\
+										<td>\
+											'+ item.instructions + '\
+										</td>\
+									</tr>\
+								').appendTo('#recipeListTableBody');
+							});
+						}
+					},
+					error: function (xhr, resp, text) {
+						console.log(xhr, resp, text);
+					}
+				});
+			});
+		});
+});
