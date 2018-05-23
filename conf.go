5d46e6d7ecad9140519e5c16e5b661230bf93cb0
Christian Olling <16271163+Olling@users.noreply.github.com>
2018-05-23 10:44:11 +0200

    Added loglevel flag

--- conf.go
+++ conf.go
@@ -20,6 +20,7 @@ type Configuration struct {
 
 var (
 	ConfigurationPath = flag.String("configurationpath","/etc/opensovs/configuration.json","(Optional) The path to the configuration file")
+	LogLevel = flag.Int("loglevel",4,"(Optional) The loglevel to apply (from Fatal(1) to Trace(6)")
 	Conf Configuration
 )
 
@@ -27,5 +28,7 @@ func InitializeConfiguration() {
 	slog.PrintDebug("Initializing configuration")
 	flag.Parse()
 	slog.PrintDebug("Configuration Path: " + *ConfigurationPath)
+	slog.SetLogLevel(*LogLevel)
+	slog.PrintDebug("Log Level:",*LogLevel)
 	ReadJsonFile(*ConfigurationPath, &Conf)
 }
